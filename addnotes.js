7494c20d78dae9e8319afa39949cd3b68c359969
hitesh975 <hiteshnachankar@gmail.com>
2026-01-19 12:02:50 +0530

    added save button functionality

--- addnotes.js
+++ addnotes.js
@@ -3,3 +3,13 @@
 document.getElementById("Back").addEventListener("click", () => {
   window.location.href = "index.html";
 });
+
+//Navigation button event listeners for add_notes page end
+
+//save button
+notes = document.getElementById("notes");
+saveBtn = document.getElementById("saveBtn");
+saveBtn.addEventListener("click", () => {
+  let notesObj = [];
+  let notesData = localStorage.getItem("notes", JSON.stringify(notesObj));
+});
